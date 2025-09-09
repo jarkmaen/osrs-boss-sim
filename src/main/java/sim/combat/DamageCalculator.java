f0635f77add3bffe6187e019fc37e2d65f8ea317
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-09-10 00:35:46 +0300

    added claw and scythe damage calculation

--- src/main/java/sim/combat/DamageCalculator.java
+++ src/main/java/sim/combat/DamageCalculator.java
@@ -1,12 +1,14 @@
 package sim.combat;
 
+import static sim.combat.RandomNumberGenerator.getRandom;
+
 public class DamageCalculator {
     public static int hit(int attack, int defence, int maxHit) {
-        int attackRoll = RandomNumberGenerator.getRandom(0, attack);
-        int defenceRoll = RandomNumberGenerator.getRandom(0, defence);
+        int attackRoll = getRandom(0, attack);
+        int defenceRoll = getRandom(0, defence);
 
         if (attackRoll > defenceRoll) {
-            int damage = RandomNumberGenerator.getRandom(0, maxHit);
+            int damage = getRandom(0, maxHit);
 
             if (damage == 0) {
                 return 1;
@@ -16,5 +18,38 @@ public class DamageCalculator {
         } else {
             return 0;
         }
+    }
+
+    public static int claw(int attack, int defence, int maxHit) {
+        int damage = 0;
+        int totalDamage = 0;
+
+        if (getRandom(0, attack) > getRandom(0, defence)) {
+            damage = getRandom(0, maxHit - 1) + maxHit;
+            totalDamage = damage / 2 + damage / 4 + damage / 8 + damage / 8 + 1;
+        } else if (getRandom(0, attack) > getRandom(0, defence)) {
+            damage = getRandom(0, maxHit - 1) + maxHit * 3 / 4;
+            totalDamage = damage / 2 + damage / 4 + damage / 4 + 1;
+        } else if (getRandom(0, attack) > getRandom(0, defence)) {
+            damage = getRandom(0, maxHit - 1) + maxHit / 2;
+            totalDamage = damage / 2 + damage / 2 + 1;
+        } else if (getRandom(0, attack) > getRandom(0, defence)) {
+            damage = getRandom(0, maxHit - 1) + maxHit / 4;
+            totalDamage = damage + 1;
+        } else if (getRandom(0, 2) < 2) {
+            totalDamage = 2;
+        }
+
+        return totalDamage;
+    }
+
+    public static int scythe(int attack, int defence, int maxHit) {
+        int damage = 0;
+
+        damage += hit(attack, defence, maxHit);
+        damage += hit(attack, defence, maxHit / 2);
+        damage += hit(attack, defence, maxHit / 4);
+
+        return damage;
     }
 }

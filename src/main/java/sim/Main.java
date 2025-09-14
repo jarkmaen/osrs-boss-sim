fb1754575efadbf08f555108b0f95f551b9849a2
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2025-09-14 23:46:12 +0300

    added basic text based UI for running simulations

--- src/main/java/sim/Main.java
+++ src/main/java/sim/Main.java
@@ -1,26 +1,10 @@
 package sim;
 
-import sim.simulations.BloatSimulation;
-import sim.simulations.DPSSimulation;
-import sim.simulations.SotetsegSimulation;
-import sim.simulations.VerzikP2Simulation;
+import sim.ui.TextUI;
 
 public class Main {
     public static void main(String[] args) {
-        int simulations = 1000000;
-
-        // BloatSimulation sim = new BloatSimulation();
-        // boolean threeDown = false;
-        // sim.run(simulations, threeDown);
-
-        // DPSSimulation sim = new DPSSimulation();
-        // sim.run(simulations);
-
-        // SotetsegSimulation sim = new SotetsegSimulation();
-        // boolean soulflameHorn = true;
-        // sim.run(simulations, soulflameHorn);
-
-        VerzikP2Simulation sim = new VerzikP2Simulation();
-        sim.run(simulations);
+        TextUI textUI = new TextUI();
+        textUI.run();
     }
 }
---
+++ src/main/java/sim/ui/TextUI.java
@@ -0,0 +1,50 @@
+package sim.ui;
+
+import java.util.Scanner;
+
+import sim.simulations.BloatSimulation;
+import sim.simulations.DPSSimulation;
+import sim.simulations.SotetsegSimulation;
+import sim.simulations.VerzikP2Simulation;
+
+public class TextUI {
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Choose a simulation to run:");
+        System.out.println("1 - Bloat Simulation");
+        System.out.println("2 - DPS Simulation");
+        System.out.println("3 - Sotetseg Simulation");
+        System.out.println("4 - Verzik P2 Simulation");
+
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+        sc.nextLine();
+
+        boolean soulflameHorn = false;
+        boolean threeDown = false;
+        int simulations = 1000000;
+
+        if (choice == 1) {
+            System.out.print("Three down? [y/n]: ");
+            threeDown = sc.nextLine().equalsIgnoreCase("y");
+        } else if (choice == 3) {
+            System.out.print("Use Soulflame Horn? [y/n]: ");
+            soulflameHorn = sc.nextLine().equalsIgnoreCase("y");
+        }
+
+        System.out.println("Running " + simulations + " simulations...");
+
+        if (choice == 1) {
+            new BloatSimulation().run(simulations, threeDown);
+        } else if (choice == 2) {
+            new DPSSimulation().run(simulations);
+        } else if (choice == 3) {
+            new SotetsegSimulation().run(simulations, soulflameHorn);
+        } else if (choice == 4) {
+            new VerzikP2Simulation().run(simulations);
+        }
+
+        sc.close();
+    }
+}
